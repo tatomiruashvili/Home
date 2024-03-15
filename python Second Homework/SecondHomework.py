6de9811d2cb7132767be027805a682464a55bcaf
tatomiruashvili <132698778+tatomiruashvili@users.noreply.github.com>
2024-03-15 14:40:44 +0400

    Add files via upload

---
+++ python Second Homework/SecondHomework.py
@@ -0,0 +1,31 @@
+from turtle import *
+
+print(7+1)
+print(1+6)
+print(5+7)      #მიმატება
+print(7+0)
+print(4+10)
+
+
+
+print(10-4)
+print(10-3)
+print(18-7)     #გამოკლება
+print(15-4)
+print(7-4)
+
+
+
+print(6*151)
+print(6*100)
+print(6*200)    #გამრავლება
+print(8*6)
+print(7*7)
+
+
+
+print(101/5)
+print(111/5)
+print(16/4)     #გაყოფა
+print(15/1)
+print(11/6)
