44c205d6b2e39cf0fd8ad36b192d1ef4cc15ba8b
tatomiruashvili <132698778+tatomiruashvili@users.noreply.github.com>
2024-03-15 14:38:58 +0400

    Add files via upload

---
+++ python Homework/homework.py
@@ -0,0 +1,12 @@
+from turtle import *
+First_name="Tato"
+Last_name="Miruashvili"
+Age="15"
+Where_are_you_from="Georgia"
+City="Gori"
+
+print(First_name)
+print(Last_name)
+print(Age)
+print(Where_are_you_from)
+print(City)
